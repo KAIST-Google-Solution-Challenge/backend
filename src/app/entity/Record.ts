eed55337641f356d1c5d137aa34f79cbc8e71f5b
Byunk <clearman001@kaist.ac.kr>
2023-02-13 23:26:18 +0900

    Modify Record Entity: set auto-generate timestamp and enum type

--- src/app/entity/Record.ts
+++ src/app/entity/Record.ts
@@ -1,22 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Feedback } from '../models/feedback.enum';
 
 @Entity()
 export class Record {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column()
-	deviceId: string;
+  @Column()
+  deviceId: string;
 
-	@Column()
-	createdAt: Date;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP()' })
+  createdAt: Date;
 
-	@Column()
-	feedback: string;
+  @Column({ type: 'enum', enum: Feedback, default: Feedback.UNKNOWN })
+  feedback: Feedback;
 
-	@Column()
-	url: string;
+  @Column()
+  url: string;
 
-	@Column()
-	isHandled: boolean;
+  @Column({ type: 'boolean', default: false })
+  isHandled: boolean;
 }
---
+++ src/app/models/feedback.enum.ts
@@ -0,0 +1,5 @@
+export enum Feedback {
+  YES,
+  NO,
+  UNKNOWN,
+}

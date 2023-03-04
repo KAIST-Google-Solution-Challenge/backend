ede92c32388bb8cfd36845bc47eb1fbb22a885bf
Byunk <clearman001@kaist.ac.kr>
2023-03-04 12:20:26 +0900

    Build audio file converter with ffmpeg to convert m4a file to wav

--- src/app/controllers/stt.controller.ts
+++ src/app/controllers/stt.controller.ts
@@ -1,52 +1,77 @@
 import { Request, Response, NextFunction } from 'express';
-import { bucket, deleteFile } from '../util/multer';
+import { bucket, deleteFile, uploadFileToBucket } from '../util/multer';
 import { format } from 'util';
 import { transcribeMp3, transcribeWav } from '../util/stt';
+import * as fs from 'fs';
+import * as ffmpeg from 'fluent-ffmpeg';
 
-export async function uploadAudio(req: Request, res: Response, next: NextFunction) {
+export async function convertAudio(req: Request, res: Response, next: NextFunction) {
   try {
     if (!req.file) {
       return res.status(400).json({ message: 'No file attached.' });
     }
-    const blob = bucket.file(req.file.originalname);
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-    });
-
-    blobStream.on('error', (err) => {
-      return res.status(500).send(err.message);
-    });
-
-    blobStream.on('finish', () => {
-      res.locals.publicUrl = format(`gs://${bucket.name}/${blob.name}`);
+    if (req.file.mimetype === 'audio/mp4') {
+      res.locals.filepath = './uploads/results/' + req.file.filename + '.wav';
+      res.locals.filename = req.file.filename + '.wav';
+      ffmpeg(req.file.path)
+        .inputFormat('m4a')
+        .outputFormat('wav')
+        .audioBitrate('44.1k')
+        .on('progress', function (progress) {
+          console.log(progress);
+        })
+        .on('error', (err) => {
+          throw new Error(err);
+        })
+        .on('end', () => {
+          fs.unlinkSync(req.file.path);
+          next();
+        })
+        .save(res.locals.filepath);
+    } else {
+      res.locals.filepath = './uploads/results/' + req.file.filename + '.mp3';
+      res.locals.filename = req.file.filename + '.mp3';
+      fs.copyFileSync(req.file.path, './uploads/results/' + req.file.filename);
+      fs.unlinkSync(req.file.path);
       next();
-    });
-    blobStream.end(req.file.buffer);
+    }
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: 'Error converting audio file' });
+  }
+}
+
+export async function uploadAudio(req: Request, res: Response, next: NextFunction) {
+  try {
+    res.locals.publicUrl = format(`gs://${bucket.name}/${res.locals.filename}`);
+    await uploadFileToBucket(res.locals.filepath);
+    next();
+  } catch (error) {
+    res.status(500).json({ message: 'Error uploading audio file' });
   }
 }
 
 export async function speechToText(req: Request, res: Response, next: NextFunction) {
   try {
-    let transcription;
+    let transcription: string;
     if (req.file.mimetype === 'audio/mpeg') {
       transcription = await transcribeMp3(res.locals.publicUrl);
-    } else if (req.file.mimetype === 'audio/wave') {
+    } else {
       transcription = await transcribeWav(res.locals.publicUrl);
     }
     res.json(transcription);
     next();
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: 'Error transcribing audio file', error: error });
+    next();
   }
 }
 
 export async function deleteAudio(req: Request, res: Response, next: NextFunction) {
   try {
-    await deleteFile(req.file.originalname);
+    fs.unlinkSync(res.locals.filepath);
+    await deleteFile(res.locals.filename);
     next();
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: 'Error deleting audio file' });
   }
 }

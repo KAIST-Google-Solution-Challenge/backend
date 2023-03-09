09431f2728e95da217908f339bd71d06a0ac90bd
Byunk <clearman001@kaist.ac.kr>
2023-03-09 17:40:20 +0900

    Change stt router to model router

--- src/app/routes/stt.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as SttController from '../controllers/stt.controller';
-import { audioUploader } from '../util/multer';
-
-const router: Router = Router();
-
-router.post('/', audioUploader.single('file'), SttController.convertAudio, SttController.uploadAudio, SttController.speechToText, SttController.deleteAudio);
-
-export const sttRouter: Router = router;

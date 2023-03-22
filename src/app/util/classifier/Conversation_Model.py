fix: softmax error

src/app/util/classifier/Conversation_Model.py
@@ -38,15 +38,7 @@ class ConversationModel:
       return train_acc
     
     def softmax(self, a):
-      a = a.tolist()
-      a.sort()
-      a0 = a[0]
-      a1 = a[1]
-      if a0 < -3 and a1 > 3:
-        a0 += 3
-        a1 -= 3
-      a = [a0, a1]
-      print(a)
+      a = a / 2
       exp_a = np.exp(a)
       sum_exp_a = np.sum(exp_a)
       y = exp_a / sum_exp_a
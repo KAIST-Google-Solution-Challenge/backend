d765e79ac44e95730b8c158b679e63c31dd296dd
Byunk <clearman001@gmail.com>
2023-05-10 20:56:14 +0900

    chore: show debug level log for local environment

--- src/app/util/logger.ts
+++ src/app/util/logger.ts
@@ -44,6 +44,7 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
+      level: 'debug',
       format: winston.format.combine(winston.format.colorize(), logFormat),
     })
   );
